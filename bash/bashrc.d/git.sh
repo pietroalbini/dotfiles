f8e48ee62a9bb3cc51ce765fe5d46cfdf706dde2
Pietro Albini <pietro@pietroalbini.io>
2015-10-14 22:34:18 +0200

    Add bash completion also for the "g" alias

--- bash/bashrc.d/git.sh
+++ bash/bashrc.d/git.sh
@@ -7,6 +7,9 @@
 source "${HOME}/.bashrc.d/external/git-completion.sh"
 source "${HOME}/.bashrc.d/external/git-prompt.sh"
 
+# Complete also the "g" alias
+__git_complete g __git_main
+
 # Add __git_ps1 to the prompt
 if type bashrc_ps1_before >/dev/null 2>&1; then
     bashrc_ps1_after "__git_ps1"
