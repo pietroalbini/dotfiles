638c62dba53d014b926f8e9c0d63572a1e1ca086
Pietro Albini <pietro@pietroalbini.org>
2018-09-25 20:06:40 +0200

    bash: always start gpg-agent

--- bash/bashrc.d/workstation/gpg-agent.sh
+++ bash/bashrc.d/workstation/gpg-agent.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Don't do anything when the agent is forwarded in SSH connections
-if [[ -z "${SSH_AUTH_SOCK}" ]]; then
-
-    # Be sure the right gpg-agent is started
-    if which gpg-agent >/dev/null 2>&1; then
-        if [[ ! -f "${HOME}/tmp/gpg-agent.env" ]]; then
-            if [[ ! -d "${HOME}/tmp" ]]; then
-                mkdir -p "${HOME}/tmp"
-            fi
-            killall gpg-agent >/dev/null 2>&1
-            gpg-agent --daemon > "${HOME}/tmp/gpg-agent.env";
+# Be sure the right gpg-agent is started
+if which gpg-agent >/dev/null 2>&1; then
+    if [[ ! -f "${HOME}/tmp/gpg-agent.env" ]]; then
+        if [[ ! -d "${HOME}/tmp" ]]; then
+            mkdir -p "${HOME}/tmp"
         fi
-        source "${HOME}/tmp/gpg-agent.env"
+        killall gpg-agent >/dev/null 2>&1
+        gpg-agent --daemon > "${HOME}/tmp/gpg-agent.env";
     fi
-
-    export GPG_AGENT_INFO
-    export SSH_AUTH_SOCK
-    export GPG_TTY=$(tty)
-
+    source "${HOME}/tmp/gpg-agent.env"
 fi
+
+export GPG_AGENT_INFO
+export SSH_AUTH_SOCK
+export GPG_TTY=$(tty)
